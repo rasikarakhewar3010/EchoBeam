ad9a5c78416f1dc87607ac0ff62c065b7c5b0713
rasikarakhewar3010 <rasikarakhewar3010@gmail.com>
2025-07-27 11:12:21 +0530

    started with frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,14 +23,17 @@ app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
     app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://rasikarakhewar30102004:xEUg6E7XdgWW40aH@cluster0.mwgvcsz.mongodb.net/")
+    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
 
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
         console.log("LISTENIN ON PORT 8000")
     });
 
+
+
 }
+
 
 
 start();
