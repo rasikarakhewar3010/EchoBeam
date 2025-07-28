d7850935e8c0c68aaa838a84b7e22a4a9d3d2fdf
rasikarakhewar3010 <rasikarakhewar3010@gmail.com>
2025-07-28 18:10:05 +0530

    video meet component completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import LandingPage from './pages/landing';
-// import Authentication from './pages/authentication';
-// import { AuthProvider } from './contexts/AuthContext';
-// import VideoMeetComponent from './pages/VideoMeet';
+import Authentication from './pages/authentication';
+import { AuthProvider } from './contexts/AuthContext';
+import VideoMeetComponent from './pages/VideoMeet';
 // import HomeComponent from './pages/home';
 // import History from './pages/history';
 
@@ -13,20 +13,20 @@ function App() {
 
       <Router>
 
-        {/* <AuthProvider> */}
+        <AuthProvider>
 
 
           <Routes>
 
             <Route path='/' element={<LandingPage />} />
 
-            {/* <Route path='/auth' element={<Authentication />} />
+            <Route path='/auth' element={<Authentication />} />
 
-            <Route path='/home's element={<HomeComponent />} />
-            <Route path='/history' element={<History />} />
-            <Route path='/:url' element={<VideoMeetComponent />} /> */}
+            {/* <Route path='/home's element={<HomeComponent />} /> */}
+            {/* <Route path='/history' element={<History />} /> */}
+            <Route path='/:url' element={<VideoMeetComponent />} />
           </Routes>
-        {/* </AuthProvider> */}
+        </AuthProvider>
 
       </Router>
     </div>
